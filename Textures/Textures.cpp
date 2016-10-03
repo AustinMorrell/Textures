7bb4db940d0950f9f400783c66da8ce16ac056c6
Austin Morrell <austinlm19@gmail.com>
2016-10-03 14:12:56 -0500

    FBX

--- Textures/Textures.cpp
+++ Textures/Textures.cpp
@@ -1,4 +1,3 @@
-#define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 #include "app.h"
 
@@ -24,24 +23,45 @@ Texture::Texture()
 	view = glm::lookAt(glm::vec3(15, 15, 15), glm::vec3(0), glm::vec3(0, 1, 0));
 	projection = glm::perspective(glm::pi<float>() * 0.25f, 16 / 9.f, 0.1f, 1000.f);
 
+	glEnable(GL_BLEND);
 	glClearColor(0.25f, 0.25f, 0.25f, 1);
 	glEnable(GL_DEPTH_TEST);
 }
 
 bool Texture::start()
 {
-	createTriQuad();
+	/*createTriQuad();*/
+	makeData();
+
+	fbx = new FBXFile();
+	fbx->load("data/models/stanford/Dragon.fbx");
+	makeModelBuffers(fbx);
 
 	int imageWidth = 0, imageHeight = 0, imageFormat = 0;
-	unsigned char* data = stbi_load("data/textures/crate.png", &imageWidth, &imageHeight, &imageFormat, STBI_rgb);
+	unsigned char* data;
 
+	data = stbi_load("data/textures/rock_diffuse.tga", &imageWidth, &imageHeight, &imageFormat, STBI_default);
 	glGenTextures(1, &m_texture);
 	glBindTexture(GL_TEXTURE_2D, m_texture);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imageWidth, imageHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-
 	stbi_image_free(data);
+
+	data = stbi_load("data/textures/rock_normal.tga", &imageWidth, &imageHeight, &imageFormat, STBI_default);
+	glGenTextures(1, &m_normal);
+	glBindTexture(GL_TEXTURE_2D, m_normal);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imageWidth, imageHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	stbi_image_free(data);
+
+	/*glGenTextures(1, &m_texture);
+	glBindTexture(GL_TEXTURE_2D, m_texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imageWidth, imageHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	stbi_image_free(data);*/
 
 	const char* vsSource;
 	std::string vs = ReadFromFile("vsInfo.txt");
@@ -51,7 +71,7 @@ bool Texture::start()
 	std::string fs = ReadFromFile("fsInfo.txt");
 	fsSource = fs.c_str();
 
-	int success = GL_FALSE;
+	/*int success = GL_FALSE;*/
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 
@@ -93,8 +113,29 @@ void Texture::draw()
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, m_texture);
 
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, m_normal);
+
 	loc = glGetUniformLocation(m_programID, "diffuse");
 	glUniform1i(loc, 0);
+
+	loc = glGetUniformLocation(m_programID, "normal");
+	glUniform1i(loc, 1);
+
+	glm::vec3 lightAD(sin(glfwGetTime()), 1, cos(glfwGetTime()));
+	loc = glGetUniformLocation(m_programID, "LightDir");
+	glUniform3f(loc, lightAD.x, lightAD.y, lightAD.z);
+
+	glBindVertexArray(p_VAO);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+	for (unsigned int i = 0; i < fbx->getMeshCount(); ++i) 
+	{
+		FBXMeshNode* mesh = fbx->getMeshByIndex(i);
+		unsigned int* glData = (unsigned int*)mesh->m_userData;
+		glBindVertexArray(glData[0]);
+		glDrawElements(GL_TRIANGLES, (unsigned int)mesh->m_indices.size(), GL_UNSIGNED_INT, 0);
+	}
 
 	glBindVertexArray(p_VAO);
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
@@ -158,4 +199,66 @@ void Texture::createTriQuad()
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+void Texture::makeData()
+{
+	Vertex vertexData[] = {
+		{ -5, 0, 5, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1 },
+		{ 5, 0, 5, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1 },
+		{ 5, 0, -5, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0 },
+		{ -5, 0, -5, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0 },
+	};
+	unsigned int indexData[] = {
+		0, 1, 2,
+		0, 2, 3,
+	};
+	glGenVertexArrays(1, &p_VAO);
+	glBindVertexArray(p_VAO);
+
+	glGenBuffers(1, &p_VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, p_VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * 4, vertexData, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &p_IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, p_IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * 6, indexData, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), ((char*)0) + 48);
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), ((char*)0) + 16);
+	glEnableVertexAttribArray(3);
+	glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), ((char*)0) + 32);
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+void Texture::makeModelBuffers(FBXFile* a)
+{
+	for (unsigned int i = 0; i < a->getMeshCount(); ++i)
+	{
+		FBXMeshNode* mesh = a->getMeshByIndex(i);
+		unsigned int* glData = new unsigned int[3];
+
+		glGenVertexArrays(1, &glData[0]);
+		glBindVertexArray(glData[0]);
+		glGenBuffers(1, &glData[1]);
+		glGenBuffers(1, &glData[2]);
+		glBindBuffer(GL_ARRAY_BUFFER, glData[1]);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, glData[2]);
+		glBufferData(GL_ARRAY_BUFFER, mesh->m_vertices.size() * sizeof(FBXVertex), mesh->m_vertices.data(), GL_STATIC_DRAW);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh->m_indices.size() * sizeof(unsigned int), mesh->m_indices.data(), GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(FBXVertex), 0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 4, GL_FLOAT, GL_TRUE, sizeof(FBXVertex), ((char*)0) + FBXVertex::NormalOffset);
+		glBindVertexArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		mesh->m_userData = glData;
+	}
 }
--- Textures/src/app.h
+++ Textures/src/app.h
@@ -9,6 +9,7 @@
 #include <string>
 #include <vector>
 #include <math.h>
+#include <FBXFile.h>
 
 class App
 {
@@ -17,6 +18,14 @@ public:
 	virtual bool update() = 0;
 	virtual void draw() = 0;
 	virtual void destroy() = 0;
+};
+
+struct Vertex 
+{
+	float x, y, z, w;
+	float nx, ny, nz, nw;
+	float tx, ty, tz, tw;
+	float s, t;
 };
 
 class Texture : public App {
@@ -28,15 +37,19 @@ public:
 	void destroy() override;
 	std::string ReadFromFile(std::string text);
 	void createTriQuad();
+	void makeModelBuffers(FBXFile* a);
+	void makeData();
 private:
 	glm::mat4 m_projectionViewMatrix;
 	glm::mat4 view;
 	glm::mat4 projection;
 	GLFWwindow* window;
+	FBXFile* fbx;
 	unsigned int m_programID;
 	unsigned int m_texture;
 	unsigned int p_VAO;
 	unsigned int p_VBO;
 	unsigned int p_IBO;
+	unsigned int m_normal;
 	int p_indicesCounter;
 };
